29be158eae9b7232a13e9d955ed67b4bea024743
Arek Cichocki <arexspeed95@gmail.com>
2021-03-14 13:19:03 +0100

    main mobile

--- client/src/components/MainLists.js
+++ client/src/components/MainLists.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import EditIcon from '@material-ui/icons/Edit';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import AddIcon from '@material-ui/icons/Add';
 import DoneIcon from '@material-ui/icons/Done';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
@@ -8,14 +9,14 @@ const MainLists = () => {
   return (
     <main className="main">
       <header className="main__header">
-        <h1 className="main__title">Shopping</h1> <span className="main__title-edit"><EditIcon /></span>
+        <h1 className="main__title lists">Shopping</h1> <button className="main__title-button button-icon lists"><EditIcon /></button> <button className="main__title-button button-icon lists"><PersonAddIcon /></button>
       </header>
       <section className="main__section">
         <div className="main__section-container">
         <h3 className="main__section-title">Items</h3>
         <article className="main__section-box">
           <div className="main__section-items">
-          <button className="main__section-item-add lists"><AddIcon /></button>
+          <button className="button-icon lists"><AddIcon /></button>
             <div className="main__section-item">
               <h5 className="main__section-item-title">Item 1</h5> <span><DoneIcon /></span>
             </div>
@@ -33,7 +34,7 @@ const MainLists = () => {
         <h3 className="main__section-title">Done</h3>
         <article className="main__section-box">
           <div className="main__section-items">
-          <button className="main__section-item-add lists"><DoneIcon /></button>
+          <button className="button-icon lists"><DoneIcon /></button>
             <div className="main__section-item">
             <span><ArrowBackIosIcon /></span> <h5 className="main__section-item-title">Item 1</h5> 
             </div>
--- client/src/components/MainProjects.js
+++ client/src/components/MainProjects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import EditIcon from '@material-ui/icons/Edit';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import AddIcon from '@material-ui/icons/Add';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
@@ -9,14 +10,14 @@ const MainProjects = () => {
   return (
     <main className="main">
       <header className="main__header">
-        <h1 className="main__title">AX Office</h1> <span className="main__title-edit"><EditIcon /></span>
+        <h1 className="main__title projects">AX Office</h1> <button className="main__title-button button-icon projects"><EditIcon /></button> <button className="main__title-button button-icon projects"><PersonAddIcon /></button>
       </header>
       <section className="main__section">
         <div className="main__section-container">
         <h3 className="main__section-title">Init</h3>
         <article className="main__section-box">
           <div className="main__section-items">
-          <button className="main__section-item-add projects"><AddIcon /></button>
+          <button className="button-icon projects"><AddIcon /></button>
             <div className="main__section-item">
               <h5 className="main__section-item-title">Task 1</h5> <span><ArrowForwardIosIcon /></span>
             </div>
@@ -34,7 +35,7 @@ const MainProjects = () => {
         <h3 className="main__section-title">In Progress</h3>
         <article className="main__section-box">
           <div className="main__section-items">
-        <button className="main__section-item-add projects"><TrendingUpIcon /></button>
+        <button className="button-icon projects"><TrendingUpIcon /></button>
             <div className="main__section-item">
               <h5 className="main__section-item-title">Task 1</h5> <span><ArrowForwardIosIcon /></span>
             </div>
@@ -52,15 +53,15 @@ const MainProjects = () => {
         <h3 className="main__section-title">Finished</h3>
         <article className="main__section-box">
           <div className="main__section-items">
-        <button className="main__section-item-add projects"><DoneIcon /></button>
+        <button className="button-icon projects"><DoneIcon /></button>
             <div className="main__section-item">
-              <h5 className="main__section-item-title">Task 1</h5> <span><ArrowForwardIosIcon /></span>
+              <h5 className="main__section-item-title">Task 1</h5> <span><DoneIcon /></span>
             </div>
             <div className="main__section-item">
-              <h5 className="main__section-item-title">Task 1</h5> <span><ArrowForwardIosIcon /></span>
+              <h5 className="main__section-item-title">Task 1</h5> <span><DoneIcon /></span>
             </div>
             <div className="main__section-item">
-              <h5 className="main__section-item-title">Task 1</h5> <span><ArrowForwardIosIcon /></span>
+              <h5 className="main__section-item-title">Task 1</h5> <span><DoneIcon /></span>
             </div>
           </div>
         </article>
