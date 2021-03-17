79fbc0fd257606af2c2c1847bce1239d0e3704be
Arek Cichocki <arexspeed95@gmail.com>
2021-03-17 12:01:26 +0100

    set sidebar projects

--- client/src/components/Projects/SidebarProjects.js
+++ client/src/components/Projects/SidebarProjects.js
@@ -1,38 +1,92 @@
-import React from 'react'
+import {useState, useEffect, useContext} from 'react'
+import { GlobalContext } from "../../GlobalProvider";
+import firebase from 'firebase'
+import db from "../../firebase";
+import {Link, useHistory} from 'react-router-dom'
 import AddIcon from '@material-ui/icons/Add';
+import SidebarBox from './SidebarBox';
 
 const SidebarProjects = ({theme}) => {
-  return (
+  const [{userInfo}] = useContext(GlobalContext)
+  const [projects, setProjects] = useState([])
+  const [userProjects, setUserProjects] = useState([])
+  const history = useHistory();
+  
+  useEffect(() => {
+
+   const unsubscribe = db.collection('projects').onSnapshot(snapshot => (
+      setProjects(
+        snapshot.docs.map(doc => ({
+          id: doc.id, 
+          name: doc.data().name,
+          users: doc.data().users,
+          timestamp: doc.data().timestamp,
+        }))
+      )
+    ))
+    return () => {
+      unsubscribe();
+    };
+  }, [])
+
+  useEffect(() => {
+    setUserProjects([])
+    projects.filter(projetc => 
+      projetc.users.find(name => name.name === userInfo.displayName) ? setUserProjects(prev => [...prev,projetc]) : ''
+    )
+   }, [projects])
+
+   const addProject = () => {
+
+    db.collection("projects").add({
+        name: 'New Project',
+        users: [{
+          id: userInfo.uid,
+          name: userInfo.displayName
+        }],
+        timestamp: new Date(),
+        timestampf: firebase.firestore.FieldValue.serverTimestamp()
+    });
+  }
+
+   return (
     <>
+    {userProjects.length >=1 ?
+    (
       <section className={`sidebar projects ${theme}`}>
         <header className="sidebar__header">
           <h2 className="sidebar__title">
             My Projects
           </h2>
-          <button className={`sidebar__add ${theme}`}><AddIcon /></button>
+          <button className={`sidebar__add ${theme}`} onClick={addProject}><AddIcon /></button>
         </header>
         
         <section className="sidebar__boxes">
-          <article className={`sidebar__box ${theme}`}>
-            <h3 className={`sidebar__box-title ${theme}`}>
-              AX Office
-            </h3>
-            <p className={`sidebar__box-subtitle ${theme}`}>20 tasks</p>
-          </article>
-          <article className={`sidebar__box ${theme}`}>
-            <h3 className={`sidebar__box-title ${theme}`}>
-              Check longer task with few words
-            </h3>
-            <p className={`sidebar__box-subtitle ${theme}`}>20 tasks</p>
-          </article>
-          <article className={`sidebar__box ${theme}`}>
-            <h3 className={`sidebar__box-title ${theme}`}>
-              AX Office
-            </h3>
-            <p className={`sidebar__box-subtitle ${theme}`}>20 tasks</p>
-          </article>
+          {userProjects.sort((a,b) => b.timestamp - a.timestamp)
+          .map(list => (
+            <SidebarBox key={list.id} id={list.id} name={list.name} users={list.users} theme={theme} />
+            ))}
         </section>
       </section>
+    )
+    :
+    (
+      <section className={`sidebar projects ${theme}`}>
+        <header className="sidebar__header">
+          <h2 className="sidebar__title">
+            My Projects
+          </h2>
+          <button className={`sidebar__add ${theme}`} onClick={addProject}><AddIcon /></button>
+        </header>
+        
+        <section className="sidebar__boxes">
+         
+        <p>Add your first list</p>
+        </section>
+      </section>
+    )  
+  }
+      
     </>
   )
 }
