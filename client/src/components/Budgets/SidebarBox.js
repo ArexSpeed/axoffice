fa6c241c69eaa580fb9ee4488fddb3abbd236581
Arek Cichocki <arexspeed95@gmail.com>
2021-03-21 23:55:10 +0100

    set inc exp in sidebar box

--- client/src/components/Budgets/SidebarBox.js
+++ client/src/components/Budgets/SidebarBox.js
@@ -6,6 +6,8 @@ import GroupIcon from '@material-ui/icons/Group';
 
 const SidebarBox = ({id,name,users, theme}) => {
   const [items, setItems] = useState([])
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
   useEffect(() => {
     if(id){
       db.collection('budgets').doc(id).collection('items').onSnapshot(snapshot => (
@@ -13,15 +15,25 @@ const SidebarBox = ({id,name,users, theme}) => {
       ))
     }
   }, [id])
+  useEffect(() => {
+    items.filter(item => item.stage === 'income' &&
+    setIncome(prev => prev + Number(item.price)) )
+    items.filter(item => item.stage === 'expense' &&
+    setExpense(prev => Number(prev) + Number(item.price)) )
+  }, [items])
 
-  console.log(items, 'items budget')
+  console.log(items, 'items budget sidebarbox')
+  console.log(income, " " ,items.length, 'income in sidebar')
   return (
     <>
     <Link to={`/budgets/${id}`} className={`sidebar__box ${theme}`}>
             <h3 className={`sidebar__box-title ${theme}`}>
               {name} {users.length > 1 && <GroupIcon />}
             </h3>
-            <p className={`sidebar__box-subtitle ${theme}`}>in {items.length}</p>
+            <p className={`sidebar__box-subtitle ${theme}`}>
+              <span style={{color: '#7be495'}}>+{income}</span>  {"  "}
+              <span style={{color: '#f44336'}}>-{expense}</span> 
+              </p>
           </Link>
     </>
   )
