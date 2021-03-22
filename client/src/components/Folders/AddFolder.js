2153a82678fad7f81f05489b0cce4f058f4b4438
Arek Cichocki <arexspeed95@gmail.com>
2021-03-22 13:38:53 +0100

    add file

---
+++ client/src/components/Folders/AddFolder.js
@@ -0,0 +1,61 @@
+import {useState} from 'react'
+import db from "../../firebase";
+import firebase from 'firebase';
+import SaveAltIcon from '@material-ui/icons/SaveAlt';
+import CancelIcon from '@material-ui/icons/Cancel';
+
+const AddFolder = ({appName, theme,id, setAddItemBox, setItems}) => {
+  const [name, setName] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addItem = () => {
+    db.collection("folders").doc(id).collection('items').add({
+      name,
+      url,
+      timestamp: new Date()
+  })
+
+    setName('')
+    setUrl('')
+    setItems([])
+    setAddItemBox(false) 
+  }
+
+  return (
+    <>
+           <div className={`formBox__container ${theme}`}>
+        <div className={`formBox__form ${theme}`}>
+            <label htmlFor="title" className="formBox__label">Name</label>
+             <input 
+               type="text"
+               className={`formBox__input ${theme}`} 
+               value={name} 
+               id="name"
+               onChange={(e) => setName(e.target.value)}
+               /> 
+              <label htmlFor="qty" className="formBox__label">url</label>
+             <input 
+               type="text"
+               className={`formBox__input ${theme}`} 
+               value={url} 
+               id="price"
+               onChange={(e) => setUrl(e.target.value)}
+               /> 
+
+
+                <div className="formBox__buttons">
+                  <button className={`button-icon ${appName}`} onClick={addItem}>
+                  <SaveAltIcon />
+                  </button>
+
+                  <button className={`button-icon-second ${appName}`} onClick={() => setAddItemBox(false)}>
+                  <CancelIcon />
+                  </button>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default AddFolder
--- client/src/components/Folders/MainFolders.js
+++ client/src/components/Folders/MainFolders.js
@@ -11,6 +11,7 @@ import SaveAltIcon from '@material-ui/icons/SaveAlt';
 import DoneIcon from '@material-ui/icons/Done';
 import Header from '../Header/Header';
 import HeaderEmpty from '../Header/HeaderEmpty';
+import AddFolder from './AddFolder';
 
 const MainFolders = ({appName,theme}) => {
   const {id} = useParams();
@@ -21,12 +22,62 @@ const MainFolders = ({appName,theme}) => {
   const [newItemName, setNewItemName] = useState('')
   const [addItemBox, setAddItemBox] = useState(false)
 
+  useEffect(() => {
+    setItems([])
+    async function fetchList(){
+      await db.collection('folders').onSnapshot(snapshot => (
+        snapshot.docs
+        .filter(doc => doc.id === id ?
+        setMyFolder([{
+          id: doc.id,
+          name: doc.data().name,
+          users: doc.data().users
+        }]
+        )
+        : '')
+      ))
+
+      await db.collection('folders').doc(id).collection('items').orderBy("name", "asc").onSnapshot(snapshot => (
+        snapshot.docs.map(doc => 
+          setItems(prev => 
+            [...prev,
+              {id: doc.id, 
+               stage: doc.data().stage, 
+               name: doc.data().name, 
+               url: doc.data().url,
+               timestamp: doc.data().timestamp
+            }])
+      )))
+      } 
+      
+        fetchList();
+
+  }, [id])
+
+  useEffect(() => {
+    myFolder.map(list => setFolderDetail(list))
+  }, [myFolder])
+
+
   return (
     <main className={`main ${theme}`}>
       {id ?
       (
       <>
         <Header appName={appName} theme={theme} collectionDetail={folderDetail} setCollectionDetail={setFolderDetail} />
+        <div className="main__section">
+          <div className="mainFolders__container">
+            <button className={`button-icon ${appName}`} onClick={() => setAddItemBox(!addItemBox)}>
+            <AddIcon />
+            </button>
+            {
+              addItemBox && <AddFolder appName={appName} theme={theme} id={id} setAddItemBox={setAddItemBox} setItems={setItems}/>
+            }
+
+            <div className="mainFolders__items"></div>
+            
+          </div>
+        </div>
       </>
       )
       :
