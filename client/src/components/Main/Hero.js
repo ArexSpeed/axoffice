f400b5db2aac5e3eb1cdc49a041388a8c203a232
Arek Cichocki <arexspeed95@gmail.com>
2021-03-23 12:24:14 +0100

    hero main

--- client/src/components/Main/Hero.js
+++ client/src/components/Main/Hero.js
@@ -1,16 +1,51 @@
-import {useState,useEffect, useCallback, useRef} from 'react'
-import img from '../../images/img.svg'
+import {useState,useEffect, useCallback} from 'react'
+import ViewListIcon from '@material-ui/icons/ViewList';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import FolderIcon from '@material-ui/icons/Folder';
+import officebudgets from '../../images/officebudgets.svg'
+import officelists from '../../images/officelists.svg'
+import officeprojects from '../../images/officeprojects.svg'
+import officefolders from '../../images/officefolders.svg'
 
 const Hero = () => {
-  const [name, setName] = useState('')
-  const spanRef = useRef('')
+  const [name, setName] = useState('lists')
+  const [icon, setIcon] = useState(<AssignmentIcon />)
+  const [image, setImage] = useState(officelists)
+  const apps = [
+    {
+      name: 'lists',
+      icon: <AssignmentIcon />,
+      img: officelists
+    },
+    {
+      name: 'projects',
+      icon: <ViewListIcon />,
+      img: officeprojects
+    }, 
+    {
+      name: 'budgets',
+      icon: <AttachMoneyIcon />,
+      img: officebudgets
+    },
+    {
+      name: 'folders',
+      icon: <FolderIcon />,
+      img: officefolders
+    },
+  ]
   const appNames = ['lists', 'projects', 'budgets', 'folders']
+  
   const nameChange = useCallback(() => {
+
     const randName= Math.floor(Math.random() * appNames.length);
-    setName(appNames[randName]);
+    setName(apps[randName].name);
+    setIcon(apps[randName].icon);
+    setImage(apps[randName].img);
   }, [setName]);
 
   useEffect(() => {
+
     const intervalId = setInterval(() => {
       nameChange()
     }, 2000);
@@ -24,15 +59,20 @@ const Hero = () => {
         <div className={`mainSite__hero-left-div ${name}`}></div>
         <p className="mainSite__hero-left-title">AX Office</p>
         <p className="mainSite__hero-left-desc">
-          One app a lot of tools <br />
+          One app a lot of <span className={`mainSite__hero-left-desc ${name}`}> tools  </span><button className={`button-icon ${name}`}>{icon}</button>  <br />
           Create and work like you want <br />
           Best option for organize all your  
-          <span className={`mainSite__hero-left-desc ${name}`} ref={spanRef}> {name}</span>
+          <span className={`mainSite__hero-left-desc ${name}`}> {name}</span>
         </p>
+        <button className={`mainSite__hero-left-button ${name}`}>
+          EXPLORE 
+          <div className={`mainSite__hero-left-button-arrow`}>&darr;</div>
+        </button>
 
       </section>
       <section className="mainSite__hero-right">
-        <img src={img} alt="" className="mainSite__hero-right-img" />
+        <img src={image} alt="" className="mainSite__hero-right-img" />
+        <div className={`mainSite__hero-right-square ${name}`}></div>
       </section>
     </div>
   )
