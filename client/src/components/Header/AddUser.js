5d8a15cf3a288e99233efa1d2b64d78fe53e32df
Arek Cichocki <arexspeed95@gmail.com>
2021-04-11 22:08:02 +0200

    help user

--- client/src/components/Header/AddUser.js
+++ client/src/components/Header/AddUser.js
@@ -2,6 +2,7 @@ import {useState, useEffect, useContext} from 'react'
 import firebase from 'firebase'
 import db from "../../firebase";
 import AddIcon from '@material-ui/icons/Add';
+import HelpIcon from '@material-ui/icons/Help';
 import { GlobalContext } from "../../GlobalProvider";
 
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
@@ -10,6 +11,7 @@ const AddUser = ({id,appName, setAddUserBox}) => {
   const [{usersList}, dispatch] = useContext(GlobalContext);
   const [searchUser, setSearchUser] = useState('')
   const [foundUser, setFoundUser] = useState([])
+  const [helpBox, setHelpBox] = useState(false);
 
   useEffect(() => {
     usersList.filter(user => user.name === searchUser && setFoundUser(user))
@@ -24,10 +26,22 @@ const AddUser = ({id,appName, setAddUserBox}) => {
   }
   return (
     <div className={`header__actionBox ${appName}`}>
-        <div className={`header__actionBox-search ${appName}`}>
+        <HelpIcon onClick={() => setHelpBox(!helpBox)}/>
+        <div 
+          onClick={() => setHelpBox(false)}
+          className={`header__actionBox-search ${appName}`} 
+        >
           <SearchOutlinedIcon />
           <input className={`header__actionBox-search input ${appName}`} type="text" placeholder="Find user" value={searchUser} onChange={(e) => setSearchUser(e.target.value)} />
-      </div>
+        </div>
+      {helpBox && 
+      (
+        <p 
+          onClick={() => setHelpBox(false)}
+          style={{margin: '20px', fontSize: '12px'}}
+        >If you want to find user you have to type the full name!</p>
+      )
+      }
       {searchUser && 
       (
         <>
